132bfacde0c357a51040b8be9b22aaef89d68eef
Tresor Sawasawa <tresorsawasawa@gmail.com>
2024-03-01 22:47:48 +0200

    update routes

--- src/utils/router.js
+++ src/utils/router.js
@@ -2,10 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import NotFound from '../pages/NotFound/NotFound';
 import Services from '../pages/Services/Services';
-import About from '../components/About/About';
+import About from '../pages/About/About';
 import Home from '../pages/Home/Home';
 import Team from '../pages/Team/Team';
 import Appointment from '../pages/Appointment/Appointment';
+import Testimonials from '../pages/Testimonials/Testimonials';
+import Contact from '../components/Contact';
 
 const router = createBrowserRouter([
   {
@@ -32,6 +34,14 @@ const router = createBrowserRouter([
       {
         path: '/appointment',
         element: <Appointment />,
+      },
+      {
+        path: '/testimonials',
+        element: <Testimonials />,
+      },
+      {
+        path: '/contact_us',
+        element: <Contact />,
       },
     ],
   },
